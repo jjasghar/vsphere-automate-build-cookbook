2814308b5c870fd2f7cfa397427c80691b3532b2
JJ Asghar <jj@chef.io>
2017-02-03 17:34:39 -0600

    added verifycation

--- metadata.rb
+++ metadata.rb
@@ -1,13 +1,10 @@
-name 'chefdk'
+name 'vsphere_demo'
 maintainer 'JJ Asghar'
 maintainer_email 'jj@chef.io'
 license 'Apache 2.0'
 description 'Installs/Configures chefdk'
 long_description 'Installs/Configures chefdk'
 version '1.0.1'
-
-issues_url 'https://github.com/jjasghar/chefdk-cookbook/issues' if respond_to?(:issues_url)
-source_url 'https://github.com/jjasghar/chefdk-cookbook/' if respond_to?(:source_url)
 
 %w(ubuntu debian redhat centos).each do |os|
   supports os
