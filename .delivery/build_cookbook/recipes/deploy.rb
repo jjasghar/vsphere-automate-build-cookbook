01fc77a376b01d87551b85f87b7cca78c3630f0c
JJ Asghar <jj@chef.io>
2017-02-06 17:20:14 -0600

    added chef exec

--- .delivery/build_cookbook/recipes/deploy.rb
+++ .delivery/build_cookbook/recipes/deploy.rb
@@ -9,8 +9,8 @@ if node['delivery']['change']['stage'] == 'rehearsal'
     cwd delivery_workspace_repo
     code <<-EOH
       STATUS=0
-      knife vsphere vm clone automate-ubuntu --template ubuntu16-template -f Linux \
-      --bootstrap --cips dhcp --dest-folder / --ssh-user admini \
+      chef exec knife vsphere vm clone automate-ubuntu --template ubuntu16-template \
+      -f Linux --bootstrap --cips dhcp --dest-folder / --ssh-user admini \
       --node-ssl-verify-mode none --ssh-password admini \
       -r 'recipe[vsphere_demo::default]' || STATUS=1
       exit $STATUS
