47da3e1794dca21e7b7fea6dbf4d2108cf4b30ba
JJ Asghar <jj@chef.io>
2017-02-06 15:28:27 -0600

    more moving of jobs

--- .delivery/build_cookbook/recipes/provision.rb
+++ .delivery/build_cookbook/recipes/provision.rb
@@ -5,17 +5,18 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
 if node['delivery']['change']['stage'] == 'acceptance'
-  bash "bootstrap a tk node" do
+  bash "bootstrap a tk node and clean up other node" do
     cwd delivery_workspace_repo
     code <<-EOH
   STATUS=0
     chef exec kitchen test || STATUS=1
+    chef exec knife vsphere vm delete automate-ubuntu -P -y || STATUS=1
     exit $STATUS
   EOH
   end
 end
 if node['delivery']['change']['stage'] == 'union'
-  bash "upload the cookbook to the chef server" do
+  bash "One last tk run to verify everything" do
   cwd delivery_workspace_repo
   code <<-EOH
   STATUS=0
