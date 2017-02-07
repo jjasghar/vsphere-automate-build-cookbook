7f3935b877dd12d7246ba08336ce67541a355ddb
JJ Asghar <jj@chef.io>
2017-02-07 12:35:49 -0600

    added it to deploy too

--- .delivery/build_cookbook/recipes/deploy.rb
+++ .delivery/build_cookbook/recipes/deploy.rb
@@ -12,7 +12,7 @@ if node['delivery']['change']['stage'] == 'rehearsal'
       chef exec knife vsphere vm clone automate-ubuntu --template ubuntu16-template \
       -f Linux --bootstrap --cips dhcp --dest-folder / --ssh-user admini \
       --node-ssl-verify-mode none --ssh-password admini \
-      -r 'recipe[vsphere_demo::default]' || STATUS=1
+      -r 'recipe[vsphere_demo::default]' --environment "#{delivery_environment}" || STATUS=1
       exit $STATUS
      EOH
   end
