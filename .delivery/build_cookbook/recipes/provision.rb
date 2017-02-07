d5861781205a3d221df148aeecb6aa048e9922d5
JJ Asghar <jj@chef.io>
2017-02-07 12:27:35 -0600

    added rehearsal section to provision

--- .delivery/build_cookbook/recipes/provision.rb
+++ .delivery/build_cookbook/recipes/provision.rb
@@ -9,7 +9,11 @@ if node['delivery']['change']['stage'] == 'acceptance'
     cwd delivery_workspace_repo
     code <<-EOH
       STATUS=0
-      chef exec kitchen test || STATUS=1
+      chef exec kitchen verify || STATUS=1
+      chef exec knife vsphere vm clone acceptance-automate-ubuntu --template ubuntu16-template \
+      -f Linux --bootstrap --cips dhcp --dest-folder / --ssh-user admini \
+      --node-ssl-verify-mode none --ssh-password admini \
+      -r 'recipe[vsphere_demo::default]' || STATUS=1
       exit $STATUS
     EOH
   end
@@ -20,6 +24,16 @@ if node['delivery']['change']['stage'] == 'union'
     code <<-EOH
       STATUS=0
       chef exec kitchen test || STATUS=1
+      exit $STATUS
+    EOH
+  end
+end
+if node['delivery']['change']['stage'] == 'rehearsal'
+  bash "One last tk run to verify everything" do
+    cwd delivery_workspace_repo
+    code <<-EOH
+      STATUS=0
+      chef exec knife vsphere vm delete acceptance-automate-ubuntu -P -y
       exit $STATUS
     EOH
   end
